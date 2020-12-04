e3fce925cf8f6f2c3610f56c02eb40c0d4bdf0ef
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-04 18:45:43 +0530

    added welcome component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import { Greet } from './Components/Greet'
+import Greet  from './Components/Greet'
 import Welcome from './Components/Welcome'
-
+import Hello from './Components/Hello'
 function App() {
   return (
     <div className="App">
      <Greet />
      <Welcome/>
+     <Hello />
     </div>
   );
 }
---
+++ src/Components/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+    return(
+        <div>
+            <h2>Hello Hashini</h2>
+        </div>
+    )
+}
+
+export default Hello 
