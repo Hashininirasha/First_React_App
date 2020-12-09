4dfad3b196df87a71ab81a04f97f97c60674fea2
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-09 17:10:15 +0530

    created Namelist.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 
 function App() {
   return (
     <div>
+      <h1>Name List</h1>
+      <hr />
+      <ul>
+        <li>Hashini - Course - Csharp</li>
+        <li>Nrasha - Course - React</li>
+        <li>Munasinghe - Course - html </li>
+      </ul>
+
+
     </div>
   );
 }
