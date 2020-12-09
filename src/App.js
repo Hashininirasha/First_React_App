d1a5b1fbd409d8c0b66dbb42a60d4069d9e8d0e9
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-09 15:59:37 +0530

    reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Greet  from './Components/Greet'
-import Welcome from './Components/Welcome'
-import Hello from './Components/Hello'
-import Msg from './Components/Msg'
-import Counter from './Components/Counter'
+
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-     <Greet  name='one' heroname='batman'>
-       <h1>This is Childern props</h1>
-     </Greet>
-     <Greet  name='one-1' heroname='superman' >
-       <button>Action</button>
-      </Greet>
-     <Welcome name='two' food='pizza'/>
-     <Hello name='three'/>
-     <Msg />
+    <div>
     </div>
   );
 }
