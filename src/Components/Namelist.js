f494425e5ed9ab584dccb3eee57d07c0c15eeeab
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-09 18:01:42 +0530

    NameListItem get to <ul>

--- src/Components/Namelist.js
+++ src/Components/Namelist.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
+import NameListItem from './NameListItem'
+
 function Namelist(){
     return(
         <div>
         <h1>Name List</h1>
         <hr />
         <ul>
-            <li>Hashini - Course - Csharp</li>
-            <li>Nrasha - Course - React</li>
-            <li>Munasinghe - Course - html </li>
+            <NameListItem />
         </ul>
         </div>
     );
