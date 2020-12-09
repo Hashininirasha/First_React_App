ef512df16ab886269bbc6c3ef0085967a719678c
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-09 17:16:03 +0530

    added Name list to Namelist.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Name List</h1>
-      <hr />
-      <ul>
-        <li>Hashini - Course - Csharp</li>
-        <li>Nrasha - Course - React</li>
-        <li>Munasinghe - Course - html </li>
-      </ul>
-
-
     </div>
   );
 }
---
+++ src/Components/Namelist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Namelist(){
+    return(){
+        <div>
+        <h1>Name List</h1>
+        <hr />
+        <ul>
+            <li>Hashini - Course - Csharp</li>
+            <li>Nrasha - Course - React</li>
+            <li>Munasinghe - Course - html </li>
+        </ul>
+        </div>
+    }
+}
