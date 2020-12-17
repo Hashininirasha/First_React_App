57c71aac829dec0aef00c501bf03bffc5d6c9373
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-17 12:53:35 +0530

    addeed avatar in imgsrc

--- src/Components/NameListItem.js
+++ src/Components/NameListItem.js
@@ -3,11 +3,11 @@ import React from 'react';
 function NameListItem(props){
     return(
         <li>
-        <p>{props.name}</p>
+        <p><img src={props.Avatar} />{props.name}</p>
         <p>{props.city}</p>
         <p>{props.email}</p>
         <p>{props.birthday}</p>
-        <p>{props.Avatar}</p>
+        
 
 
         </li>
