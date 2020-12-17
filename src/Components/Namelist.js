2bac0a8359981501dddb3f25c18e6a7d3a6e2162
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-17 12:47:37 +0530

    addeed birthday and avatar

--- src/Components/Namelist.js
+++ src/Components/Namelist.js
@@ -28,11 +28,14 @@ function Namelist(){
         <h1>Name List</h1>
         <hr />
         <ul>
-            
-            <NameListItem  name='Hashini' course="html" email="hasi@gmail.com"/>
-            <NameListItem  name="Nirasha" course="css" email="nira@gmail.com"/>
-            <NameListItem  name="Munasinghe" course="java" email="muna@gmail.com"/>
-            <NameListItem  name="Dauna" course="c#" email="dasu@gmail.com"/>
+             <NameListItem  
+             name='Hashini' 
+             city="html" 
+             email="hasi@gmail.com"
+             birthday=""
+             Avatar=""
+             />
+           
         </ul>
         </React.Fragment>
     );
