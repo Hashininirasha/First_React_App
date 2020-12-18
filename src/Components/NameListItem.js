f0f26e0f0ece839edfe0fa380682dd32d208f938
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-18 17:53:54 +0530

    import red color to photo and name

--- src/Components/NameListItem.js
+++ src/Components/NameListItem.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import moment from 'moment'
+import './NameListItem.css'
+
+
 function NameListItem(props){
     return(
         <li>
-        <p>Photo : <img src={props.Avatar} alt={props.name} /><br/><br/>Name : {props.name}</p>
+        <p className='redtext'>Photo : <img src={props.Avatar} alt={props.name} /><br/><br/>Name : {props.name}</p>
         <p>City : {props.city}</p>
         <p>Email : {props.email}</p>
         <p>Birthday : {moment(props.dob).format('DD-MM-YYYY')}</p>
