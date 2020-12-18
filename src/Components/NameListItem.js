4355a5c4c46561b5e9a0ff3a31e34469976a6406
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-18 17:56:40 +0530

    use inline css to city

--- src/Components/NameListItem.js
+++ src/Components/NameListItem.js
@@ -7,7 +7,7 @@ function NameListItem(props){
     return(
         <li>
         <p className='redtext'>Photo : <img src={props.Avatar} alt={props.name} /><br/><br/>Name : {props.name}</p>
-        <p>City : {props.city}</p>
+        <p style={{color:'green'}}>City : {props.city}</p>
         <p>Email : {props.email}</p>
         <p>Birthday : {moment(props.dob).format('DD-MM-YYYY')}</p>
         
