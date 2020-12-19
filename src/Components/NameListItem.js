2935fad80dfd4fab2536d17573baf9a6aeb02921
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-19 11:37:44 +0530

    added row and col to alignmets

--- src/Components/NameListItem.js
+++ src/Components/NameListItem.js
@@ -6,13 +6,17 @@ import './NameListItem.css'
 function NameListItem(props){
     return(
         <li className="list-group-item">
-        <p className='redtext'>Photo : <img src={props.Avatar} alt={props.name} /><br/><br/>Name : {props.name}</p>
-        <p style={{color:'green'}}>City : {props.city}</p>
-        <p>Email : {props.email}</p>
-        <p>Birthday : {moment(props.dob).format('DD-MM-YYYY')}</p>
-        
-
-
+            <div className="row">
+                <div className="col-md">
+                <img src={props.Avatar} alt={props.name} />
+                </div>
+                <div className="col-sm">
+                <p>Name : {props.name}</p>
+                <p>City : {props.city}</p>
+                <p>Email : {props.email}</p>
+                <p>Birthday : {moment(props.dob).format('DD-MM-YYYY')}</p>
+                </div>
+            </div>
         </li>
     );
 }
