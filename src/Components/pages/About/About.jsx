a58efcc57deadee957b76b6e80aed7bafb46d0e9
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-30 22:02:58 +0530

    created About.jsx

---
+++ src/Components/pages/About/About.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+function About(){
+    return(
+        <>
+            <div className='container mt-3'>
+                <h3>About</h3>
+                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+            </div>
+
+        </>
+    );
+}
+export default About;
