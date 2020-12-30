29141667d1e1bee81376213c21f2648092711780
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-30 19:58:35 +0530

    added navigation bar items

--- src/Components/Headrbar/Header.jsx
+++ src/Components/Headrbar/Header.jsx
@@ -1,8 +1,14 @@
 import React  from 'react';
 function Header(){
     return(
-        <nav className="navbar navbar-light bg-light shadow">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
             <span className="navbar-brand mb-0 h1"> Name_List </span>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact Us</a></li>
+
+            </ul>
         </nav>
     );
 }
