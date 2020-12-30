d2dc69d0113909e9d8e41cac95d067943d6623e4
Hashininirasha <hashininirashamunasinghe@gmail.com>
2020-12-30 20:04:03 +0530

    added classname nav-link

--- src/Components/Headrbar/Header.jsx
+++ src/Components/Headrbar/Header.jsx
@@ -4,9 +4,9 @@ function Header(){
         <nav className="navbar navbar-expand-lg navbar-light bg-light shadow">
             <span className="navbar-brand mb-0 h1"> Name_List </span>
             <ul className="navbar-nav">
-                <li className="nav-item"><a href="#">Home</a></li>
-                <li className="nav-item"><a href="#">About</a></li>
-                <li className="nav-item"><a href="#">Contact Us</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Contact Us</a></li>
 
             </ul>
         </nav>
