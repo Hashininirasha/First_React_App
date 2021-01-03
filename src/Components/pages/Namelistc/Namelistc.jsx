916ee8005ea0f8a07f152618f446bd3e471ef00e
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-01-03 21:48:42 +0530

    created Namelistc folder and file

---
+++ src/Components/pages/Namelistc/Namelistc.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Namelistc extends Component{
+
+    render(){
+        return(
+            <h3>Class Component </h3>
+        )
+
+    }
+
+}
+export default Namelistc;
+
